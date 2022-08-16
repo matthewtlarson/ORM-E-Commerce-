98ca4ef6299e514bde5705cccd55495dc6106cd3
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-08-16 12:40:51 -0700

    product routes second get

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,6 +28,30 @@ router.get('/', (req, res) => {
 
 // get one product
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {id: req.params.id},
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [{
+      model: Category,
+      attributes: ['id', 'category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['id', 'tag_name']
+    }
+  ]
+  })
+  .then(dbData => {
+    if (!dbData) {
+      res.status(400).json({ message: 'ID could not be found' });
+      return;
+    }
+  res.json(dbData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
