a186a0977c4f736c1f4ffb08538a691b98479fe4
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-08-16 09:32:19 -0700

    category get route

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # E-commerce Back End Starter Code
-hi
+in progress
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,6 +4,18 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+  })
+  .then(dbData => res.json(dbData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // find all categories
   // be sure to include its associated Products
 });
